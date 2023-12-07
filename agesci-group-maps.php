a52c11c2378d4caa6a8deff69360eb9271d903fa
Eugenio Tampieri <webmaster@emiro.agesci.it>
2023-12-07 08:18:19 +0100

    Initial commit

---
+++ agesci-group-maps.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: AGESCI Mappe gruppi
+ * Plugin URI: https://github.com/eutampieri/agesci-group-maps
+ * Description: Memorizza in WordPress i dati dei gruppi
+ * Version: 0.1
+ * Requires at least: 4.9
+ * Requires PHP: 7.0
+ * Author: Eugenio Tampieri
+ * Author URI: https://github.com/eutampieri
+ *
+ */
+
