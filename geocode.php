bfab47021e4c7641f925df40d5b275e7b4f6c3f3
Eugenio Tampieri <eugenio@eutampieri.eu>
2024-02-14 12:34:21 +0100

    Fix geocoding

--- geocode.php
+++ geocode.php
@@ -1,5 +1,5 @@
 <?php
 function agd_geocode($address, $mapbox_token) {
-	$response = file_get_contents("https://api.mapbox.com/search/geocode/v6/forward?&address_number=".urlencode($address["cap_gruppo"])."&street=".urlencode($address["indirizzo_gruppo"])."&postcode=".urlencode($address["cap_gruppo"])."&country=IT&proximity=ip&limit=1&access_token=".$mapbox_token);
+	$response = file_get_contents("https://api.mapbox.com/search/geocode/v6/forward?&address_number=".urlencode($address["civico_gruppo"])."&street=".urlencode($address["indirizzo_gruppo"])."&postcode=".urlencode($address["cap_gruppo"])."&country=IT&proximity=ip&limit=1&access_token=".$mapbox_token);
 	return json_decode($response, true)["features"][0]["geometry"]["coordinates"];
 }
