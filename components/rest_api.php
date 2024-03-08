4b356d87ddb25eb2fa41f3b8ea85346b11676a62
Eugenio Tampieri <webmaster@emiro.agesci.it>
2024-03-08 16:51:38 +0100

    Add new driving optimized mode

--- components/rest_api.php
+++ components/rest_api.php
@@ -9,7 +9,7 @@ function agd_get_zones() {
 function agd_get_groups_in_zone($data) {
     global $wpdb;
     $table_name = $wpdb->prefix . 'group_data';
-    $result = $wpdb->get_results($wpdb_prepare("SELECT * FROM $table_name WHERE REPLACE(REPLACE(LOWER(zone), \" \", \"\"), \"zona\", \"\")=%s;", $data["zone"]));
+    $result = $wpdb->get_results($wpdb->prepare("SELECT * FROM $table_name WHERE REPLACE(REPLACE(LOWER(zone), \" \", \"\"), \"zona\", \"\")=%s;", $data["zone"]));
     return $result;
 }
 
@@ -20,7 +20,19 @@ function agd_get_k_nearest_groups() {
     $k = min(10, intval($_GET["k"]));
     $lat = floatval($_GET["lat"]);
     $lon = floatval($_GET["lon"]);
-    $result = $wpdb->get_results("SELECT *, (latitude - $lat)*(latitude - $lat) + (longitude - $lon) * (longitude - $lon) as distance FROM $table_name WHERE showtime>=$ts ORDER BY distance LIMIT $k;");
+	if(isset($_GET["calculate_duration"])) {
+		$k = $k * 2;
+	}
+    $result = $wpdb->get_results("SELECT *, (latitude - $lat)*(latitude - $lat) + (longitude - $lon) * (longitude - $lon) as distance FROM $table_name ORDER BY distance LIMIT $k;");
+	if(isset($_GET["calculate_duration"])) {
+		$token = get_option('agd_mapbox_key');
+		foreach($result as $r) {
+			$url = "https://api.mapbox.com/directions/v5/mapbox/driving/{$r->longitude}%2C{$r->latitude}%3B{$lon}%2C{$lat}?alternatives=false&geometries=geojson&overview=simplified&steps=false&notifications=none&access_token={$token}";
+			$r->duration = json_decode(file_get_contents($url), true)["routes"][0]["duration"] / 60;
+		}
+		usort($result, function($a, $b) { return $a->duration - $b->duration;});
+		$result = array_slice($result, 0, $k / 2);
+	}
     return $result;
 }
 
